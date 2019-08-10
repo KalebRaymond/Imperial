678189b98d54cd7b1df227ee2e952bf35a823a50
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2019-08-10 14:50:00 -0400

    Add files via upload

--- search_text.js
+++ search_text.js
@@ -1,7 +1,8 @@
 /*Code adapted from https://github.com/TriMill/text_replacer (which in turn was adapted from https://github.com/labrose/webextensions-examples )*/
 /*KNOWN ISSUES: Doesn't put newlines back where they elong
 				Puts extraneous spaces into code blocks on webpages (github, stack, etc)
-				Doesn't check for cases where there is no space btw number & unit (ex 100kg) */
+				Doesn't check for cases where there is no space btw number & unit (ex 100kg) 
+				Makes youtube crash*/
 				
 function isNumber(n)
 {
@@ -24,9 +25,43 @@ function replaceText (node)
 		node.textContent  += content[0] + " "; 
 		for(var i = 1; i < content.length; ++i)
 		{	
-			if(content[i] === "kg" && isNumber(content[i - 1]))
+			if(isNumber(content[i - 1]))
 			{
-				content[i] = content[i].replace("kg", "kg (" + content[i - 1] * 2.205 + " lbs)");
+				switch(content[i].toLowerCase())
+				{
+					case "kg":
+						content[i] = content[i].replace("kg", "kg (" + (content[i - 1] * 2.205).toFixed(4) + " lbs)");
+						break;
+					case "g":
+						content[i] = content[i].replace("g", "g (" + (content[i - 1] * 0.0353).toFixed(2) + " oz)");
+						break;
+					case "km":
+						content[i] = content[i].replace("km", "km (" + (content[i - 1] * 0.6214).toFixed(4) + " mi)");
+						break;
+					case "m":
+						content[i] = content[i].replace("m", "m (" + (content[i - 1] * 3.2808).toFixed(4) + " ft)");
+						break;
+					case "cm":
+						if(parseInt(content[i - 1], 10) > 50)
+						{
+							var inches = content[i - 1] * 0.393701;
+							var feet = Math.floor(inches / 12);
+							var inches = Math.round(inches % 12);
+							
+							content[i] = content[i].replace("cm", "cm (" + feet + "'" + inches + "\")");
+						}
+						else
+						{
+							content[i] = content[i].replace("cm", "cm (" + (content[i - 1] * 0.3937).toFixed(4) + " in)");
+						}
+						break;
+					case "km/h":
+						content[i] = content[i].replace("km/h", "km/h (" + (content[i - 1] * 0.6214).toFixed(4) + " mph)");
+						break;
+					case "kmh":
+						content[i] = content[i].replace("kmh", "kmh (" + (content[i - 1] * 0.6214).toFixed(4) + " mph)");
+						break;
+				}
 			}
 			
 			node.textContent  += content[i] + " "; 
