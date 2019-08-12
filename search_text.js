169c1c4aa0b72c3b54b95c68716c0329b6c8bd2d
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2019-08-12 16:00:46 -0400

    Add files via upload

--- search_text.js
+++ search_text.js
@@ -1,8 +1,5 @@
 /*Code adapted from https://github.com/TriMill/text_replacer (which in turn was adapted from https://github.com/labrose/webextensions-examples )*/
-/*KNOWN ISSUES: Doesn't put newlines back where they elong
-				Puts extraneous spaces into code blocks on webpages (github, stack, etc)
-				Doesn't check for cases where there is no space btw number & unit (ex 100kg) 
-				Makes youtube crash*/
+/*KNOWN ISSUES: Totally breaks websites */
 				
 function isNumber(n)
 {
@@ -15,41 +12,51 @@ function isNumber(n)
 //Returned string does not contain the original metric value.
 function getConversion(num, unit)
 {
+	var newLineFlag = true;
+	
+	if(!unit.includes("~|?"))
+	{	
+		newLineFlag = false;
+		unit += "~|?";
+	}
 	switch(unit.toLowerCase())
 	{
-		case "kg":
-			unit = unit.replace("kg", "kg (" + (num * 2.205).toFixed(4) + " lbs)");
+		case "kg~|?":
+			unit = unit.replace("kg~|?", "kg (" + (num * 2.205).toFixed(4) + " lbs)");
 			break;
-		case "g":
-			unit = unit.replace("g", "g (" + (num * 0.0353).toFixed(2) + " oz)");
+		case "g~|?":
+			unit = unit.replace("g~|?", "g (" + (num * 0.0353).toFixed(2) + " oz)");
 			break;
-		case "km":
-			unit = unit.replace("km", "km (" + (num * 0.6214).toFixed(4) + " mi)");
+		case "km~|?":
+			unit = unit.replace("km~|?", "km (" + (num * 0.6214).toFixed(4) + " mi)");
 			break;
-		case "m":
-			unit = unit.replace("m", "m (" + (num * 3.2808).toFixed(4) + " ft)");
+		case "m~|?":
+			unit = unit.replace("m~|?", "m (" + (num * 3.2808).toFixed(4) + " ft)");
 			break;
-		case "cm":
+		case "cm~|?":
 			if(parseInt(num, 10) > 50)
 			{
 				var inches = num * 0.393701;
 				var feet = Math.floor(inches / 12);
 				var inches = Math.round(inches % 12);
 				
-				unit = unit.replace("cm", "cm (" + feet + "'" + inches + "\")");
+				unit = unit.replace("cm~|?", "cm (" + feet + "'" + inches + "\")");
 			}
 			else
 			{
-				unit = unit.replace("cm", "cm (" + (num * 0.3937).toFixed(4) + " in)");
+				unit = unit.replace("cm~|?", "cm (" + (num * 0.3937).toFixed(4) + " in)");
 			}
 			break;
-		case "km/h":
-			unit = unit.replace("km/h", "km/h (" + (num * 0.6214).toFixed(4) + " mph)");
+		case "km/h~|?":
+			unit = unit.replace("km/h~|?", "km/h (" + (num * 0.6214).toFixed(4) + " mph)");
 			break;
-		case "kmh":
-			unit = unit.replace("kmh", "kmh (" + (num * 0.6214).toFixed(4) + " mph)");
+		case "kmh~|?":
+			unit = unit.replace("kmh~|?", "kmh (" + (num * 0.6214).toFixed(4) + " mph)");
 			break;
 	}
+	
+	if(newLineFlag)
+		unit += "~|?";
 	
 	return unit;
 }
@@ -63,12 +70,13 @@ function replaceText (node)
 			return;
 		}
 							   
-		let content = node.textContent.replace(/\n/g, " ").split(" "); //Replace newlines with spaces, separate text into array of words
+		let content = node.textContent.replace(/\n/g, "~|? ").split(" "); //Replace newlines with spaces, separate text into array of words
 		node.textContent = "";
 		
 		//Iterate over words on page, add conversions where needed, put text back into node.textContent
 		for(var i = 0; i < content.length; ++i)
 		{	
+			
 			if(i > 0 && isNumber(content[i - 1]))
 			{
 				content[i] = getConversion(content[i - 1], content[i]);
@@ -91,7 +99,12 @@ function replaceText (node)
 				content[i] = num + getConversion(num, unit);
 			}
 			
-			node.textContent  += content[i] + " ";
+			if(content[i].includes("~|?"))
+				content[i] = content[i].replace("~|?", "\n");
+			else
+				content[i] += " ";
+			
+			node.textContent += content[i];
 		}
 	}
 	
